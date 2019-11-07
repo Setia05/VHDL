eaabfa0d6403af2437d747c2a00b15e2e7c98870
Shubham Setia <54857010+Setia05@users.noreply.github.com>
2019-11-07 01:06:25 -0500

    Create nor_gate.vhd

---
+++ nor_gate.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+ 
+entity nor_gate is
+Port( A : in std_logic;
+		B : in std_logic;
+		C : in std_logic;
+		Y : out std_logic);
+end nor_gate;
+architecture Behavioral of and_gate is
+begin
+
+	Y<=(A nor B);
+	
+end Behavioral;
